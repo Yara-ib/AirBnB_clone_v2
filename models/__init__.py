32b3dfbe7f6ca5e9001489ca026f8a3afac3e317
JNRBK <rafeekmorgan@yahoo.com>
2024-03-21 02:53:23 +0200

    con

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+    This module instantiates an object
+    of class FileStorage or DBStorage
+"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
 
-storage = FileStorage()
-storage.reload()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+    storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
---
+++ models/engine/db_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" DBStorage Module """
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.base_model import Base
+
+
+class DBStorage():
+    __engine = None
+    __session = None
+
+    def __init__(self) -> None:
+        self.__engine = create_engine('''
+            mysql+mysqldb://
+            getenv('HBNB_MYSQL_USER'):
+            getenv('HBNB_MYSQL_PWD')
+            @getenv('HBNB_MYSQL_HOST')/
+            getenv('HBNB_MYSQL_DB')''',
+            pool_pre_ping=True
+            )
+
+    def all(self, cls=None):
+        """ Query on the current database session """
+
+
+    def new(self, obj):
+        """ Add the object to the current db session """
+        self.__session.add(obj)
+
+    def save(self):
+        """ Commit all changes of the current db session """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ Delete from the current database session """
+        if obj:
+            del obj
+            self.__session.commit()
+
+    def reload(self):
+        """ Finishing the work """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        Base.metadata.create_all(DBStorage.__engine)
+
+        self.__session = sessionmaker(
+            sessionmaker(bind=self.__engine,
+                         expire_on_commit=False))
