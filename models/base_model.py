9cfca1601c5ecf511e676b6cb45ae7e2ff37c292
Yara-ib <yr.ap.23@gmail.com>
2024-03-23 01:19:45 +0200

    Table

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,22 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime, func
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column('id', String(60), primary_key=True)
+    created_at = Column(
+        'created_at', DateTime, default=func.now(), nullable=False)
+    updated_at = Column(
+        'updated_at', DateTime, default=func.now(), nullable=False
+        )
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,7 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +43,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
