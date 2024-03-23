930802d4abcb1726b6420baba1df962530e93596
Yara-ib <yr.ap.23@gmail.com>
2024-03-23 22:03:42 +0200

    Import

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, relationship
+from models.base_model import BaseModel, Base, Column, String, relationship
 
 
 class User(BaseModel, Base):
