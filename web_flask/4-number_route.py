8c9c6b4e7a28f81d987e182d7cbc6c8046d0f662
Yara-ib <yr.ap.23@gmail.com>
2024-04-21 22:38:23 +0200

    N is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+""" Number this time HBNB or Hello HBNB! or C is fun! Maybe Python? Module """
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    Method to display “Hello HBNB!”
+
+    Returns:
+        string: Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """
+    Method to display “HBNB”
+
+    Returns:
+        string: HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun_or_cool(text):
+    """
+    Method to display “C + string”
+
+    Returns:
+        string: C + string
+    """
+    text_replaced = text.replace("_", " ")
+    return f"C {escape(text_replaced)}"
+
+
+@app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python_more_fun(text):
+    """
+    Method to display “Python + string”
+
+    Returns:
+        string: Python + string
+    """
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    Method to display “n is a number”
+
+    Returns:
+        string: n is a number
+    """
+    return f"{escape(n)} is a number"
+
+
+if __name__ == "__main__":
+    """ Changing port & host Flask listen to """
+    app.run(debug=True, host='0.0.0.0', port=5000)
