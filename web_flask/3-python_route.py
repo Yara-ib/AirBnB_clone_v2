5830c751d5a695a08e79c45a82ea4f505c17d58e
Yara-ib <yr.ap.23@gmail.com>
2024-04-21 20:03:57 +0200

    Adding new Route option

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -40,6 +40,7 @@ def c_is_fun_or_cool(text):
     text_replaced = text.replace("_", " ")
     return f"C {escape(text_replaced)}"
 
+
 @app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
 @app.route("/python/<text>", strict_slashes=False)
 def python_more_fun(text):
